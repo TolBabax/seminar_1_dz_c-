0aeddabc43bfa3baadc7983e23fc12b2f41cc3e2
TolBabax <kuz.cerg@gmail.com>
2023-01-23 18:50:12 +0400

    добавление программы. определение четного и нечетного числа.

---
+++ task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number_remains = number % 2;
+if(number_remains == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+else Console.WriteLine($"{number} - нечетное число");
